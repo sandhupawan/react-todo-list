3d40a7ae65199ea7fb309ef809c4c0a31bcea55c
Pawan Sandhu <sandhu@Pawans-MacBook-Air.local>
2019-05-26 11:57:06 +0530

    Setting Up App Com

--- src/App.js
+++ src/App.js
@@ -5,12 +5,49 @@ import TodoInputs from "./Components/TodoInputs";
 import TodoList from "./Components/TodoList";
 
 class App extends Component {
+  state = {
+    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    console.log("handle change");
+  };
+  handleSubmit = e => {
+    console.log("handle submit");
+  };
+  clearList = () => {
+    console.log("clear list");
+  };
+
+  handleEdit = id => {
+    console.log(`handle edit ${id}`);
+  };
+
+  handleDelete = id => {
+    console.log(`handle delete ${id}`);
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
-          <TodoInputs />
-          <TodoList />
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-captilize text-center">todo list</h3>
+            <TodoInputs
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleEdit={this.handleEdit}
+              handleDelete={this.handleDelete}
+            />
+          </div>
         </div>
       </div>
     );
