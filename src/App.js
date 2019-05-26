18e9e06e58d292930c579d6ecd1f090aa754fc92
Pawan Sandhu <sandhu@Pawans-MacBook-Air.local>
2019-05-26 14:01:05 +0530

    Finished Application

--- src/App.js
+++ src/App.js
@@ -32,15 +32,27 @@ class App extends Component {
   };
 
   clearList = () => {
-    console.log("clear list");
+    this.setState({
+      items: []
+    });
   };
 
   handleEdit = id => {
-    console.log(`handle edit ${id}`);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    const selectedItem = this.state.items.find(item => item.id === id);
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true
+    });
   };
 
   handleDelete = id => {
-    console.log(`handle delete ${id}`);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    });
   };
 
   render() {
